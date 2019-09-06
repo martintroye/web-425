577b49f83ab30d126a9e52bb3ef0a434e4f29fa6
Troy Martin <troyemartin@gmail.com>
2019-09-06 06:01:43 -0500

    Assignment 1.4

---
+++ week-1/martin-assignment-1_4.js
@@ -0,0 +1,42 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Troy Martin
+; Date: 09/06/2019
+; Modified By: Troy Martin
+; Description: TypeScript
+;===========================================
+*/
+// start program
+/*
+; Expected output:
+; Troy Martin
+; Assignment 1.4
+; {{date time}}
+;
+; Hello Fred Flintstone
+;
+*/
+// variable declaration and assignment
+// declare and set the date time for the header output
+var firstName = "Fred";
+var lastName = "Flintstone";
+// function declaration
+/*
+; Params: firstName: string, lastName: string
+; Response: Hello {firstName} {lastName}
+; Description: Return a message using the provided first and last name
+*/
+function myName(firstName, lastName) {
+    // return the message concatenating the first and last name
+    return "Hello " + firstName + " " + lastName;
+}
+// output
+// Call the console log function and output a well-formatted header
+console.log("Troy Martin");
+console.log("Assignment 1.4");
+console.log(new Date().toLocaleDateString('en-US'));
+console.log("");
+// Call the console log function and output the result of the myName function
+console.log(myName(firstName, lastName));
+// end program
