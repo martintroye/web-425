e6bc9bbbc41f8d8aae37e990b2391160db44276f
Troy Martin <troyemartin@gmail.com>
2019-09-14 06:20:48 -0500

    2.3 Two-Way Binding

--- week-2/martin-two-way-binding/src/app/app.module.ts
+++ week-2/martin-two-way-binding/src/app/app.module.ts
@@ -1,18 +1,40 @@
+/*
+============================================
+; Title: Exercise 2.3
+; Author: Troy Martin
+; Date: 09/13/2019
+; Modified By: Troy Martin
+; Description: Two-Way Binding
+;===========================================
+*/
+// start program
+// import the angular platform browser module
 import { BrowserModule } from '@angular/platform-browser';
+// import the angular core module
 import { NgModule } from '@angular/core';
+// import the angular forms module, brings in ngModel
 import { FormsModule} from '@angular/forms';
 
+// import the app component
 import { AppComponent } from './app.component';
 
+// declare the module
 @NgModule({
+  // declarations
   declarations: [
     AppComponent
   ],
+  // imports
   imports: [
     BrowserModule,
     FormsModule
   ],
+  // services and other providers
   providers: [],
+  // component to start the application
   bootstrap: [AppComponent]
 })
+// declare and export the module class
 export class AppModule { }
+
+// end program
