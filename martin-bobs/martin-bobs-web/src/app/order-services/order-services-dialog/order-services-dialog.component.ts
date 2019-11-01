ad97c43ec89a74a272ca31a8b0f31a4f95a727f8
Troy Martin <troy.martin@fcsamerica.com>
2019-11-01 14:38:23 -0500

    9.3 WIP

--- martin-bobs/martin-bobs-web/src/app/order-services/order-services-dialog/order-services-dialog.component.ts
+++ martin-bobs/martin-bobs-web/src/app/order-services/order-services-dialog/order-services-dialog.component.ts
@@ -1,38 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductOfferingService } from '../product-offering.service';
 import { ProductOffering } from '../models/product-offering.model';
 import { FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 
+@AutoUnsubscribe()
 @Component({
   templateUrl: './order-services-dialog.component.html',
   styleUrls: ['./order-services-dialog.component.scss']
 })
-export class OrderServicesDialogComponent implements OnInit {
-
+export class OrderServicesDialogComponent implements OnInit, OnDestroy {
   products: ProductOffering[];
   orderGroup: FormGroup;
+  valueChangesSubscription: Subscription;
+  orderGrandTotal = 0.00;
 
-  constructor(private productOfferingService: ProductOfferingService, private formBuilder: FormBuilder) {  }
+  constructor(private productOfferingService: ProductOfferingService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.productOfferingService.getProducts().subscribe((products) => {
       this.products = products;
+
       const formControls = this.products.map(control => new FormControl(false));
-      this.orderGroup = this.formBuilder.group({ productOfferings: new FormArray(formControls)});
+      this.orderGroup = this.formBuilder.group({ productOfferings: new FormArray(formControls) });
+
+      this.valueChangesSubscription = this.orderGroup.valueChanges.subscribe((productOfferings) => {
+        const selectedServices = this.getSelectedServices();
+        this.orderGrandTotal = 0.00;
+        selectedServices.map((productOffering) => {
+          this.orderGrandTotal += productOffering.price;
+        });
+      });
     });
   }
 
+  ngOnDestroy(): void { }
+
   toggleService(index: number) {
-    const control = ( this.orderGroup.controls.productOfferings as FormArray).controls[index];
+    const control = (this.orderGroup.controls.productOfferings as FormArray).controls[index];
     control.setValue(!control.value);
     this.products[index].selected = control.value;
   }
 
-  submit() {
-    const selectedServices = this.orderGroup.value.productOfferings
-    .map((checked, index) => checked ? this.products[index] : null)
+  getSelectedServices(): ProductOffering[] {
+    return this.orderGroup.value.productOfferings
+    .map((checked, index) => this.setProduct(checked, index))
     .filter(value => value !== null);
+  }
 
+  setProduct(checked: boolean, index: number) {
+    this.products[index].selected = checked;
+    return checked ? this.products[index] : null;
+  }
+
+  submit() {
+    const selectedServices = this.getSelectedServices();
     console.log(selectedServices);
   }
 }
