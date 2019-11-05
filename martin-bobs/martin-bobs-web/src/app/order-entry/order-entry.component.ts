bf4c4fc55df656e6934be2919a400d77ded13712
Troy Martin <troyemartin@gmail.com>
2019-11-04 23:30:58 -0600

    9.3 WIP

--- martin-bobs/martin-bobs-web/src/app/order-entry/order-entry.component.ts
+++ martin-bobs/martin-bobs-web/src/app/order-entry/order-entry.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductOffering } from '../shared/models/product-offering.model';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-order-entry',
@@ -9,10 +10,16 @@ import { ProductOffering } from '../shared/models/product-offering.model';
 export class OrderEntryComponent implements OnInit {
   selectedServices: ProductOffering[] = [];
   orderGrandTotal = 0.00;
+  entryGroup: FormGroup;
 
-  constructor() { }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.entryGroup = this.formBuilder.group({});
+    this.entryGroup.addControl('laborHours', new FormControl(''));
+    this.entryGroup.addControl('partsTotal', new FormControl(''));
+
   }
   setTotal(total: number) {
     this.orderGrandTotal = total;
