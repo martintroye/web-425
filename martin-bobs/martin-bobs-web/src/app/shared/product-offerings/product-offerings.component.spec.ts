7329ccff84dcf19b60652980f93dd3bbbafe3c62
Troy Martin <troyemartin@gmail.com>
2019-11-06 22:00:23 -0600

    9.2 Testing

--- martin-bobs/martin-bobs-web/src/app/shared/product-offerings/product-offerings.component.spec.ts
+++ martin-bobs/martin-bobs-web/src/app/shared/product-offerings/product-offerings.component.spec.ts
@@ -1,14 +1,31 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop
+; Author: Troy Martin
+; Date: 11/05/2019
+; Modified By: Troy Martin
+; Description: WEB425
+;===========================================
+*/
+// imports from the testing module
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// imports from the forms module
 import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+// imports from the material module
 import {MatCheckboxModule, MatChipsModule} from '@angular/material';
+// import our custom product offering component
 import { ProductOfferingsComponent } from './product-offerings.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('ProductOfferingsComponent', () => {
   let component: ProductOfferingsComponent;
   let fixture: ComponentFixture<ProductOfferingsComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      // imports to handle custom elements
       imports: [
         FormsModule,
         ReactiveFormsModule,
@@ -25,10 +42,81 @@ describe('ProductOfferingsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductOfferingsComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  // test that component can be created
+  it('should create the component', () => {
+    // the component was created
     expect(component).toBeTruthy();
   });
+
+  // test that we are rendering the correct number of checkboxes
+  it('should render 7 checkboxes', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    // get the checkboxes being rendered
+    const elements = compiled.querySelectorAll('.mat-checkbox-input');
+    // should be a checkbox for each product
+    expect(elements.length).toEqual(component.products.length);
+  });
+
+  // if you select a checkbox is the value correct
+  it('select checkbox to order service', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    // ge the checkbox elements
+    const elements = compiled.querySelectorAll('.mat-checkbox-input');
+    // we find the first element
+    expect(elements[0]).toBeTruthy();
+    // it is currently false, unchecked
+    expect(elements[0].checked).toBeFalsy();
+    // clicking the checkbox checks it
+    elements[0].click();
+    // detect changes to the dom
+    fixture.detectChanges();
+    // the checkbox is now checked
+    expect(elements[0].checked).toBeTruthy();
+    // the product in the array is marked selected
+    expect(component.products[0].selected).toBeTruthy();
+
+    // click the element again to turn it off
+    elements[0].click();
+    // detect changes in the dom
+    fixture.detectChanges();
+    // element is no longer checked
+    expect(elements[0].checked).toBeFalsy();
+    // the product in the array is no longer selected
+    expect(component.products[0].selected).toBeFalsy();
+
+  });
+
+  // when the checkbox changes the order total changes
+  it('selected service changes total', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    // ge the checkbox elements
+    const elements = compiled.querySelectorAll('.mat-checkbox-input');
+    // we find the first element
+    expect(elements[0]).toBeTruthy();
+    // it is currently false, unchecked
+    expect(elements[0].checked).toBeFalsy();
+    // clicking the checkbox checks it
+    elements[0].click();
+    // detect changes to the dom
+    fixture.detectChanges();
+    // the checkbox is now checked
+    expect(elements[0].checked).toBeTruthy();
+    // since the service is selected the price is correct
+    expect(component.orderGrandTotal).toEqual(component.products[0].price);
+
+    // click the element again to turn it off
+    elements[0].click();
+    // detect changes in the dom
+    fixture.detectChanges();
+    // element is no longer checked
+    expect(elements[0].checked).toBeFalsy();
+    // order total is correct
+    expect(component.orderGrandTotal).toEqual(0);
+
+  });
+
 });
--- martin-bobs/martin-bobs-web/src/app/shared/services/order-invoice.service.spec.ts
+++ martin-bobs/martin-bobs-web/src/app/shared/services/order-invoice.service.spec.ts
@@ -1,45 +1,74 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop
+; Author: Troy Martin
+; Date: 11/05/2019
+; Modified By: Troy Martin
+; Description: WEB425
+;===========================================
+*/
+// imports from the testing module
 import { TestBed } from '@angular/core/testing';
+// import our customer order invoice service
 import { OrderInvoiceService } from './order-invoice.service';
+// import our customer order model
 import { CustomerOrder } from '../models/customer-order.model';
+// import our invoice summary model
 import { InvoiceSummary } from '../models/invoice-summary.model';
+
 
 describe('OrderInvoiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
+  // create a default customer order model
   const order = new CustomerOrder();
   order.repairs = 'Clean hard drive';
   order.selectedServices = [
       {description: 'test', selected: true, price: 1, icon: 'test'}
     ];
 
+  // create a default invoice summary model
   const invoice = new InvoiceSummary();
   invoice.id = 1;
   invoice.services = [
     {description: 'test', selected: true, price: 1, icon: 'test'}
   ];
 
+  // test that service is created
   it('should be created', () => {
     const service: OrderInvoiceService = TestBed.get(OrderInvoiceService);
+    // test that service is created
     expect(service).toBeTruthy();
   });
 
+  // test that the submitOrder method stores the customer order correctly
   it('submit a customer order, stored properly', () => {
     const service: OrderInvoiceService = TestBed.get(OrderInvoiceService);
 
+    // call the submitOrder method
     service.submitOrder(order);
+    // retrieve the order from the service
     service.getCurrentOrder().subscribe((o) => {
+      // test that order is not null
       expect(o).toBeTruthy();
+      // test that the order is the same as we stored
       expect(o.selectedServices.length).toEqual(1);
     });
   });
 
+  // test that the createInvoice method stores the invoice summary correctly
   it('create an invoice, stored properly', () => {
     const service: OrderInvoiceService = TestBed.get(OrderInvoiceService);
 
+    // call the createInvoice method
     service.createInvoice(invoice);
+    // retrieve the invoice
     service.getInvoiceById(1).subscribe((i) => {
+      // test that the invoice is not null
       expect(i).toBeTruthy();
+      // test that the id is the same
       expect(i.id).toEqual(1);
+      // test that the services array has the same number of items
       expect(i.services.length).toEqual(1);
     });
   });
